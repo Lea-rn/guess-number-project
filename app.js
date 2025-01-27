5a5484167eaa334aaeca96605e207bb543c9685c
achrefmechergui <achreffcb123achref@gamil.com>
2025-01-27 22:10:03 +0100

    html-css

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// console.log(document) //// el file html ...
+
+// let buttonAgain = document.querySelector(".btn-again").textContent = "jalila"
+// console.log(buttonAgain)
+
+// let logo = document.querySelector(".result-logo")
+// console.log(logo)
+
+// let input = document.querySelector("input").value
+// console.log(input)
+
+// let title = document.querySelector(".title").textContent ="welcome"
+// console.log(title)
+
+////// may7otech number ; number shih ; number ghalet(number akber , asgher)
+
+let secretNumber = Math.trunc(Math.random()*20+1) ; 
+console.log(secretNumber)
+
+
+
+ document.querySelector(".check").addEventListener("click",function(){
+console.log("hello")
+ })
